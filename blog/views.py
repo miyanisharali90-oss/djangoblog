6cee70f55958e05b947430776ff5c039a354aa55
miyanisharali90-oss <miyanisharali90@gmail.com>
2026-09-11 15:05:29 +0900

    css base.html, 2026-9-11

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'blog/home.html', {'title': 'This is the Djangoblog Homepage.'})
+
+def about(request):
+    return render(request, 'blog/about.html', {'content': 'This is the Djangoblog team.'})
+
+def contact(request):
+    return render(request, 'blog/contact.html', {'abc': 'This is the Djangoblog Contact.'})
+
+

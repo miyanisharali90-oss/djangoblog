1489215cccb0a92a96dcc396882803b7156af9b3
miyanisharali90-oss <miyanisharali90@gmail.com>
2026-09-18 13:56:03 +0900

    Set up admin interface and Debug Toolbar; seed sample posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+    filter_horizontal = ("tags",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
-def home(request):
-    return render(request, 'blog/home.html', {'title': 'This is the Djangoblog Homepage.'})
-
-def about(request):
-    return render(request, 'blog/about.html', {'content': 'This is the Djangoblog team.'})
-
-def contact(request):
-    return render(request, 'blog/contact.html', {'abc': 'This is the Djangoblog Contact.'})
-
-
+def post_list(request):
+    posts = Post.objects.filter(status="published").order_by("-created_at")
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "blog/post_list.html", {"page_obj": page_obj})
